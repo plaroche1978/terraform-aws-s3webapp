0b485b0b77fc1f6397dc8488b7c8f6ae5bfd84ab
Pascal Laroche <pascal.laroche@hashicorp.com>
2024-04-08 11:20:53 -0400

    owner preferred

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
 resource "aws_s3_bucket_ownership_controls" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
